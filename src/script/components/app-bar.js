6f5fad121c0e7c5419912196b356707eec4cab93
Muhammad Afif <afif.maliki21@gmail.com>
2022-09-18 12:02:07 +0700

    Club Finder First Commit

---
+++ src/script/components/app-bar.js
@@ -0,0 +1,11 @@
+class AppBar extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `<h2>Club Finder</h2>`;
+  }
+}
+
+customElements.define("nav-bar", AppBar);
